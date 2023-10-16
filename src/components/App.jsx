136d66963cc79c6204a194392c6165fb0706cc2a
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-10-16 23:44:40 +0300

    comm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,21 @@ import data from './Statistics/data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from './FriendList/friends.json';
 
+import { Transactions } from './Transactions/TransactionHistory';
+import transactions from './Transactions/transactions.json';
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
       <Profile
         username={user.username}
@@ -29,6 +33,7 @@ export const App = () => {
      
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+     <TransactionHistory items={transactions} />;
     </div>
   );
 };
